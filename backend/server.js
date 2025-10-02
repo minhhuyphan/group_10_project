875bd1a0527bdedace90e1a6075e5c94c7cd555f
huynhchiphuc <huynhchiphuchcp@gmail.com>
2025-10-02 14:48:43 +0700

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
+
+const userRoutes = require('./routes/user');
+app.use('/', userRoutes);
+
 const POST = process.env.PORT || 3000;
 app.listen(POST, () => {
   console.log(`Server is running on port ${POST}`);
